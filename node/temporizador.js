2bf46c1a73f1e6e69e88cf7ebac584c735d3e42a
Eduardo Batista Pereira <duuhpwn@gmail.com>
2022-09-02 21:41:55 -0300

    Temporizador, Entradas e Saídas

---
+++ node/temporizador.js
@@ -0,0 +1,22 @@
+const schedule = require('node-schedule')
+
+const tarefa1 = schedule.scheduleJob('*/5 * * * * *', function () {
+  console.log('Executando Tarefa 1!', new Date().getSeconds())
+}) // Executa a qulquer dia e a qualquer hora
+
+setTimeout(function () {
+  tarefa1.cancel
+  console.log('Cancelando Tarefa 1')
+}, 20000)
+
+// setImmediate
+// setInterval - Dispara função de tempos em tempos
+
+const regra = new schedule.RecurrenceRule()
+regra.dayOfWeek = [new schedule.Range(1, 5)]
+regra.hour = 12
+regra.second = 30
+
+const tarefa2 = schedule.scheduleJob(regra, function () {
+  console.log('Executando Tarefa 2!', new Date().getSeconds())
+})
